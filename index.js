52e8c7ef3c39ba6eca95d4fc2505907da75be195
Shubham Yadav <shubhamya.1157@gmail.com>
2026-08-05 01:13:33 +0530

    basic testing

--- index.js
+++ index.js
@@ -6,11 +6,39 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+
 const redis = new Redis(process.env.REDIS_URL);
+
+const BANNER_KEY = "app:banner";
+
+app.post("/banner" , async(req,res)=>{
+
+    await redis.set(BANNER_KEY,req.body.message || "Welcome back");
+    res.json({success : true})
+
+})
+
+app.get("/banner",async (req,res)=>{
+    const message = await redis.get(BANNER_KEY);
+    res.json({message});
+
+})
+
+app.delete("/banner",async (req,res)=>{
+    await redis.del(BANNER_KEY);
+    res.json({success : true})
+})
+
+app.get("/banner/exists",async (req,res)=>{
+    const exist = await redis.exists(BANNER_KEY);
+    res.json({exist:Boolean(exist)})
+})
+
+
 
 
 app.get("/",async(req,res)=>{
-    
     res.send("hello from express server")
 })
 
